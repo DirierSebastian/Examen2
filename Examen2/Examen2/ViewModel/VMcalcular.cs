e5918579e5e211b668f00a450fa9c14fb88c2471
DirierSebastian <sebastianagvzla@gmail.com>
2024-02-01 14:15:25 -0700

    SIN TRIGGERS

--- Examen2/Examen2/ViewModel/VMcalcular.cs
+++ Examen2/Examen2/ViewModel/VMcalcular.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -17,13 +18,15 @@ namespace Examen2.ViewModel
         bool _Vfcn;
         double _Peso;
         double _Altura;
-        double _Latidos;
+        int _Latidos;
+        bool _Img1;
+        bool _Img2;
+        bool _R;
         #endregion
         #region CONSTRUCTOR
         public VMcalcular(INavigation navigation)
         {
             Navigation = navigation;
-            
         }
         #endregion
         #region OBJETOS
@@ -32,15 +35,51 @@ namespace Examen2.ViewModel
             get { return _Texto; }
             set { SetValue(ref _Texto, value); }
         }
+        public bool R
+        {
+            get { return _R; }
+            set { SetValue(ref _R, value); }
+        }
+        public bool Img1
+        {
+            get { return _Img1; }
+            set { SetValue(ref _Img1, value); }
+        }
+        public bool Img2
+        {
+            get { return _Img2; }
+            set { SetValue(ref _Img2, value); }
+        }
         public bool Condicion1
         {
             get { return _Condicion1; }
-            set { SetValue(ref _Condicion1, value); }
+            set { SetValue(ref _Condicion1, value);
+                if (_Condicion1 == true)
+                {
+                    Vimc = true;
+                    Vfcn = false;
+                }
+                else
+                {
+                    Calcular();
+                }
+            }
         }
         public bool Condicion2
         {
             get { return _Condicion2; }
-            set { SetValue(ref _Condicion2, value); }
+            set { SetValue(ref _Condicion2, value);
+                if (_Condicion1 == false)
+                {
+                    Vimc = false;
+                    Vfcn = true;
+                }
+                else
+                {
+                    Calcular();
+                }
+
+            }
         }
         public bool Vimc
         {
@@ -62,7 +101,7 @@ namespace Examen2.ViewModel
             get { return _Altura; }
             set { SetValue(ref _Altura, value); }
         }
-        public double Latidos
+        public int Latidos
         {
             get { return _Latidos; }
             set { SetValue(ref _Latidos, value); }
@@ -80,16 +119,27 @@ namespace Examen2.ViewModel
             {
                 Vimc = true;
                 Vfcn = false;
+                Img1 = false;
+                Img2 = false;
+                R = false;
+
             }
             else if (_Condicion2 == true)
             {
                 Vimc = false;
                 Vfcn = true;
+                Img1 = false;
+                Img2 = false;
+                R = false;
             }
             else
             {
                 Vimc = false;
                 Vfcn = false;
+                Img1 = false;
+                Img2 = false;
+                R = false;
+
             }
         }
 
@@ -101,31 +151,70 @@ namespace Examen2.ViewModel
             if(total <= 18.5)
             {
                 await DisplayAlert("IMC", "Peso insuficiente", "OK");
+                R = true;
+                Img1 = true;
+                Img2 = false;
             }
             else if(total >18.5 &&  total <= 24.9)
             {
                 await DisplayAlert("IMC", "Peso normal", "OK");
+                R = true;
+                Img1 = false;
+                Img2 = true;
             }
             else if(total >24.9 && total <= 29.9)
             {
                 await DisplayAlert("IMC", "Sobrepeso", "OK");
+                R = true;
+                Img1 = true;
+                Img2 = false;
             }
             else
             {
                 await DisplayAlert("IMC", "Obesidad", "OK");
+                R = true;
+                Img1 = true;
+                Img2 = false;
             }
         }
 
-
-
+        public async Task FCN()
+        {
+            int latidos = Latidos;
+            int resultado = latidos * 4;
+            if(resultado < 60)
+            {
+                await DisplayAlert("FCN", "FC baja", "OK");
+                Img1 = true;
+                Img2 = false;
+                R = true;
+            }
+            else if (resultado >= 60 && resultado < 100)
+            {
+                await DisplayAlert("FCN", "FC normal", "OK");
+                Img1 = false;
+                Img2 = true;
+                R = true;
+            }
+            if (resultado >= 100)
+            {
+                await DisplayAlert("FCN", "FC alto", "OK");
+                Img1 = true;
+                Img2 = false;
+                R = true;
+            }
+        }
+        
         public void procesoSimple()
         {
 
         }
         #endregion
         #region COMANDOS
+
         public ICommand ProcesoAsyncomand => new Command(async () => await ProcesoAsyncrono());
         public ICommand IMCcommand => new Command(async () => await IMC());
+        public ICommand FCNcommand => new Command(async () => await FCN());
         public ICommand ProcesoSimpcomand => new Command(procesoSimple);
         #endregion
     }
